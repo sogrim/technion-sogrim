cdb47dd450c1afcd016c38d38d68fc5ee2e06b2e
benny-n <benick12@gmail.com>
2021-09-14 02:28:11 +0300

    Add auth with google

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,19 @@ use rocket::http::{Cookie, CookieJar, SameSite};
 use rocket::{routes};
 use rocket::response::{Debug, Redirect};
 use rocket_oauth2::{OAuth2, TokenResponse};
+use serde_json::{self, Value};
 
 /// User information to be retrieved from the GitHub API.
 #[derive(serde::Deserialize)]
 struct GitHubUserInfo {
     #[serde(default)]
     name: String,
+}
+
+/// User information to be retrieved from the Google People API.
+#[derive(serde::Deserialize)]
+struct GoogleUserInfo {
+    names: Vec<Value>,
 }
 
 #[get("/login/github")]
@@ -44,6 +51,57 @@ async fn github_callback( token: TokenResponse<GitHubUserInfo>, cookies: &Cookie
     Ok(Redirect::to("/login"))
 }
 
+#[get("/login/google")]
+fn google_login(oauth2: OAuth2<GoogleUserInfo>, cookies: &CookieJar<'_>) -> Redirect {
+    oauth2.get_redirect(cookies, &["profile"]).unwrap()
+}
+
+#[get("/auth/google")]
+async fn google_callback(
+    token: TokenResponse<GoogleUserInfo>,
+    cookies: &CookieJar<'_>,
+) -> Result<Redirect, Debug<Error>> {
+    // Use the token to retrieve the user's Google account information.
+    let response = reqwest::Client::builder()
+        .build()
+        .context("failed to build reqwest client")?
+        .get("https://people.googleapis.com/v1/people/me?personFields=names")
+        .header(AUTHORIZATION, format!("Bearer {}", token.access_token()))
+        .send()
+        .await
+        .context("failed to complete request")?;
+
+    let respone_txt = response.text().await.unwrap_or("couldn't unwrap response".to_string());
+    println!("{}", respone_txt);
+
+    let user_info : GoogleUserInfo = reqwest::Client::builder()
+        .build()
+        .context("failed to build reqwest client")?
+        .get("https://people.googleapis.com/v1/people/me?personFields=names")
+        .header(AUTHORIZATION, format!("Bearer {}", token.access_token()))
+        .send()
+        .await
+        .context("failed to complete request")?   
+        .json()
+        .await
+        .context("failed to deserialize response")?;   
+
+    let real_name = user_info
+        .names
+        .first()
+        .and_then(|n| n.get("displayName"))
+        .and_then(|s| s.as_str())
+        .unwrap_or("");
+
+    // Set a private cookie with the user's name, and redirect to the home page.
+    cookies.add_private(
+        Cookie::build("username", real_name.to_string())
+            .same_site(SameSite::Lax)
+            .finish(),
+    );
+    Ok(Redirect::to("/login"))
+}
+
 #[get("/login")]
 fn successful_login (cookies: &CookieJar<'_>) -> String{
     let token = cookies.get_private("username");
@@ -69,6 +127,8 @@ fn rocket() -> _ {
     println!("Starting rocket app at http://{}:{}", external_ip_addr, std::env::var("ROCKET_PORT").unwrap_or("no port".to_string()));
 
     rocket::build()
-        .mount("/", routes![github_callback, github_login, successful_login, index])
+        .mount("/", routes![github_callback, github_login, 
+        google_callback, google_login, successful_login, index])
         .attach(OAuth2::<GitHubUserInfo>::fairing("github"))
+        .attach(OAuth2::<GoogleUserInfo>::fairing("google"))
 }
