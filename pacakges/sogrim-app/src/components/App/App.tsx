3cebda927b9f98d2e3880347d1c27a6600dcfe3d
Nissan Ohana <nissano@wix.com>
2021-10-08 15:36:46 +0300

    install critical packages

--- pacakges/sogrim-app/src/components/App/App.tsx
+++ pacakges/sogrim-app/src/components/App/App.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="App-header">        
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          אלוף ישראל בבוקס
         </p>
         <a
           className="App-link"
--- pacakges/sogrim-app/src/index.tsx
+++ pacakges/sogrim-app/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
@@ -11,7 +11,5 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// TODO: check about this
 reportWebVitals();
