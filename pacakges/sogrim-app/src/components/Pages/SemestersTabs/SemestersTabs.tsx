9acf9f12ebf5a948553d92bc063a8ffd3ef7c471
nissano <nissano@wix.com>
2021-11-22 12:19:31 +0200

    overflow msg comp.

---
+++ pacakges/sogrim-app/src/components/Pages/SemestersTabs/SemestersTabs.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { observer } from 'mobx-react-lite';
+import useUserState from '../../../hooks/apiHooks/useUserState';
+import { CourseStatus } from '../../../types/data-types';
+
+interface SemestersTabProps {
+}
+
+const SemestersTabComp: React.FC<SemestersTabProps> = () => {
+
+    const { data: userState }  = useUserState();
+
+    const allCourses: CourseStatus[] = userState?.details?.degree_status?.course_statuses || [];
+    
+    return (
+        <div> 
+            { allCourses.map( (course, id) => <h1> {course.course.name} - {course.state} </h1>)}
+          
+        </div>
+      
+    );
+};
+
+
+
+export const SemestersTab = observer(SemestersTabComp);
+
