e06992e3fe4117bf80bf4fdbbb8a67e4c738f67b
nissano <nissano@wix.com>
2021-11-26 17:29:14 +0200

    comment all old buttons

--- pacakges/sogrim-app/src/components/Banner/BannerTitle.tsx
+++ pacakges/sogrim-app/src/components/Banner/BannerTitle.tsx
@@ -1,19 +1,22 @@
 import { Box, Theme, Typography } from "@mui/material";
 import React, { useEffect } from "react";
 import { observer } from 'mobx-react-lite';
-import { FormModal } from "../Commom/FormModal";
-import { SogrimButton } from "../Commom/SogrimButton";
+
 import { useStore } from "../../hooks/useStore";
-import { ExportGilion } from "./BannerDialogs/ExportGilion";
-import { SelectCatalog } from "./BannerDialogs/SelectCatalog";
 import { useAuth } from "../../hooks/useAuth";
 import useComputeEndGame from "../../hooks/apiHooks/useComputeEndGame";
 
+// import { FormModal } from "../Commom/FormModal";
+// import { SogrimButton } from "../Commom/SogrimButton";
+// import { ExportGilion } from "./BannerDialogs/ExportGilion";
+// import { SelectCatalog } from "./BannerDialogs/SelectCatalog";
+
+
 export const BannerTitleComp: React.FC = () => {
-  const [coursesModalOpen, coursesModalsetOpen] = React.useState(false);  
-  const [catalogsModalOpen, catalogsModalsetOpen] = React.useState(false);  
+  // const [coursesModalOpen, coursesModalsetOpen] = React.useState(false);  
+  // const [catalogsModalOpen, catalogsModalsetOpen] = React.useState(false);  
   const [triggerCompute, setTriggerCompute] = React.useState(false);  
-  
+
   const { uiStore: {
     userDisplyName,
   }} = useStore();
@@ -31,25 +34,25 @@ export const BannerTitleComp: React.FC = () => {
   }, [data, isLoading, isError]);
 
 
-  const coursesHandleClickOpen = () => {
-    coursesModalsetOpen(true);
-  };
+  // const coursesHandleClickOpen = () => {
+  //   coursesModalsetOpen(true);
+  // };
 
-  const coursesHandleClose = () => {
-    coursesModalsetOpen(false);
-  };
+  // const coursesHandleClose = () => {
+  //   coursesModalsetOpen(false);
+  // };
 
-  const catalogsHandleClickOpen = () => {
-    catalogsModalsetOpen(true);
-  };
+  // const catalogsHandleClickOpen = () => {
+  //   catalogsModalsetOpen(true);
+  // };
 
-  const catalogsHandleClose = () => {
-    catalogsModalsetOpen(false);
-  };
+  // const catalogsHandleClose = () => {
+  //   catalogsModalsetOpen(false);
+  // };
 
-  const handleTriggerCompute = () => {
-    setTriggerCompute(true);
-  }
+  // const handleTriggerCompute = () => {
+  //   setTriggerCompute(true);
+  // }
   
   
   return <Box>
@@ -57,7 +60,7 @@ export const BannerTitleComp: React.FC = () => {
              {`שלום ${userDisplyName ?? ','}`}
         </Typography>       
         <Box sx={{display: 'flex', flexDirection: 'row' ,marginTop: '10px', gap: '10px'}}> 
-        <SogrimButton onClick={handleTriggerCompute}>
+        {/* <SogrimButton onClick={handleTriggerCompute}>
           סגור את התואר!
         </SogrimButton>       
         <SogrimButton onClick={coursesHandleClickOpen}>
@@ -67,7 +70,7 @@ export const BannerTitleComp: React.FC = () => {
           בחר קטלוג
         </SogrimButton>
         <FormModal dialogContent={<ExportGilion handleClose={coursesHandleClose} />} handleClose={coursesHandleClose} open={coursesModalOpen}/>
-        <FormModal dialogContent={<SelectCatalog handleClose={catalogsHandleClose} />} handleClose={catalogsHandleClose} open={catalogsModalOpen}/>
+        <FormModal dialogContent={<SelectCatalog handleClose={catalogsHandleClose} />} handleClose={catalogsHandleClose} open={catalogsModalOpen}/> */}
         </Box>
   </Box> 
 }
