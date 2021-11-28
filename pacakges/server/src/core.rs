a1682dce0f6acfa2c65826f410bb71139452914c
liad <liadaram1@gmail.com>
2021-11-28 16:49:47 +0200

    moving struct Grade to course.rs

--- pacakges/server/src/core.rs
+++ pacakges/server/src/core.rs
@@ -1,12 +1,11 @@
 use std::collections::HashMap;
 use bson::doc;
-use serde::de::{Error, Unexpected, Visitor};
-use serde::{Deserialize, Deserializer, Serialize, Serializer};
+use serde::{Deserialize, Serialize};
 use petgraph::Graph;
 use petgraph::algo::toposort;
 use crate::catalog::Catalog;
 use crate::user::UserDetails;
-use crate::course::{Course, CourseState, CourseStatus, CourseBank};
+use crate::course::{Course, CourseState, CourseStatus, CourseBank, Grade};
 
 type Chain = Vec<u32>;
 type NumCourses = u32;
@@ -64,62 +63,6 @@ impl ToString for Rule{
             Rule::SpecializationGroups(_) => "specialization groups".into(),
             Rule::Wildcard(_) => "wildcard".into(),
         }
-    }
-}
-
-#[derive(Clone, Debug)]
-pub enum Grade{
-    Grade(u8),
-    Binary(bool),
-    ExemptionWithoutCredit,
-    ExemptionWithCredit,
-}
-
-impl Serialize for Grade {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        match *self{
-            Grade::Grade(grade) => serializer.serialize_str(grade.to_string().as_str()),
-            Grade::Binary(val) if val => serializer.serialize_str("עבר"),
-            Grade::Binary(_) => serializer.serialize_str("נכשל"),
-            Grade::ExemptionWithoutCredit => serializer.serialize_str("פטור ללא ניקוד"),
-            Grade::ExemptionWithCredit => serializer.serialize_str("פטור עם ניקוד"),
-        }    
-    }
-}
-struct StrVisitor;
-
-impl<'de> Visitor<'de> for StrVisitor {
-    type Value = Grade;
-
-    fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
-        formatter.write_str("a valid string representation of a grade")
-    }
-
-    fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
-    where
-        E: Error,
-    {
-        match v{
-            "עבר" => Ok(Grade::Binary(true)),
-            "נכשל" => Ok(Grade::Binary(false)),
-            "פטור ללא ניקוד" => Ok(Grade::ExemptionWithoutCredit),
-            "פטור עם ניקוד" => Ok(Grade::ExemptionWithCredit),
-            _ if v.parse::<u8>().is_ok() => Ok(Grade::Grade(v.parse::<u8>().unwrap())),
-            _ => Err(Error::invalid_type(Unexpected::Str(v), &self))
-        }
-        
-    }
-}
-
-impl<'de> Deserialize<'de> for Grade {
-    fn deserialize<D>(deserializer: D) -> Result<Grade, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        deserializer.deserialize_str(StrVisitor)
     }
 }
 
