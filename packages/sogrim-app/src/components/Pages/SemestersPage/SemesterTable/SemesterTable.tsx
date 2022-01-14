65fee7869647744352323d8a05870a4b7e6d7966
nissano <nissano@wix.com>
2022-01-14 16:10:09 +0200

    prettier install

--- packages/sogrim-app/src/components/Pages/SemestersPage/SemesterTable/SemesterTable.tsx
+++ packages/sogrim-app/src/components/Pages/SemestersPage/SemesterTable/SemesterTable.tsx
@@ -1,29 +1,23 @@
-import { useState, useEffect } from 'react';
-import {Box, Table, TableContainer, Button} from '@mui/material';
-import { observer } from 'mobx-react-lite';
-import { RowData } from './SemesterTabsConsts';
-import { Paper } from '@mui/material';
-import { SemesterTableHeader } from './SemesterTableHeader';
-import { useStore } from '../../../../hooks/useStore';
-import { useAuth } from '../../../../hooks/useAuth';
-import useUserState from '../../../../hooks/apiHooks/useUserState';
-import useComputeEndGame from '../../../../hooks/apiHooks/useComputeEndGame';
-import useUpdateUserState from '../../../../hooks/apiHooks/useUpdateUserState';
-import { SemesterTableBody } from './SemesterTableBody';
-import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
+import { useState, useEffect } from "react";
+import { Box, Table, TableContainer, Button } from "@mui/material";
+import { observer } from "mobx-react-lite";
+import { RowData } from "./SemesterTabsConsts";
+import { Paper } from "@mui/material";
+import { SemesterTableHeader } from "./SemesterTableHeader";
+import { useStore } from "../../../../hooks/useStore";
+import { useAuth } from "../../../../hooks/useAuth";
+import useUserState from "../../../../hooks/apiHooks/useUserState";
+import useComputeEndGame from "../../../../hooks/apiHooks/useComputeEndGame";
+import useUpdateUserState from "../../../../hooks/apiHooks/useUpdateUserState";
+import { SemesterTableBody } from "./SemesterTableBody";
 
-export interface SemesterTableProps {    
-    semester: string;    
-  }
+export interface SemesterTableProps {
+  semester: string;
+}
 
-const SemesterTableComp: React.FC<SemesterTableProps> = ({    
-    semester,
-}) => {
-
-    const { dataStore: {            
-            generateRows,
-            updateCourseInUserDetails,            
-          }
+const SemesterTableComp: React.FC<SemesterTableProps> = ({ semester }) => {
+  const {
+    dataStore: { generateRows, updateCourseInUserDetails },
   } = useStore();
 
   const { userAuthToken } = useAuth();
@@ -35,45 +29,64 @@ const SemesterTableComp: React.FC<SemesterTableProps> = ({
 
   useEffect(() => {
     if (data) {
-      setTableRows(generateRows(semester, data?.details.degree_status.course_statuses));      
+      setTableRows(
+        generateRows(semester, data?.details.degree_status.course_statuses)
+      );
     }
-  },[ data, isLoading, generateRows, semester ]);
+  }, [data, isLoading, generateRows, semester]);
 
   const handleSave = (newRowData: RowData, semester: string) => {
     if (!isLoading && data && data?.details) {
-      const newUserDetails = updateCourseInUserDetails(newRowData, semester, data?.details);      
+      const newUserDetails = updateCourseInUserDetails(
+        newRowData,
+        semester,
+        data?.details
+      );
       mutate(newUserDetails);
-      console.log('hi hi hi' , data?.details.degree_status.course_statuses)
-      const newnewrow = generateRows(semester, data?.details.degree_status.course_statuses);
-      console.log('~~~~~~ NEW! ', newnewrow);
-      setTableRows(newnewrow);      
+      console.log("hi hi hi", data?.details.degree_status.course_statuses);
+      const newnewrow = generateRows(
+        semester,
+        data?.details.degree_status.course_statuses
+      );
+      console.log("~~~~~~ NEW! ", newnewrow);
+      setTableRows(newnewrow);
     }
-  }
+  };
 
   const handleRowToggle = () => {
     setAddRowToggle(!addRowToggle);
-  }
-    
+  };
+
   return (
-    <Box sx={{ width: '100%', display: 'flex', alignItems: 'center', flexDirection: 'column'}}>
-      <Paper sx={{ width: '100%', mb: 2 }}>        
-        <TableContainer sx={{ width: '1200px' }}>
-          <Table            
-            aria-labelledby="tableTitle"
-            size={'small'}
-          >
-            <SemesterTableHeader                
+    <Box
+      sx={{
+        width: "100%",
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <Paper sx={{ width: "100%", mb: 2 }}>
+        <TableContainer sx={{ width: "1200px" }}>
+          <Table aria-labelledby="tableTitle" size={"small"}>
+            <SemesterTableHeader />
+            <SemesterTableBody
+              tableRows={tableRows}
+              semester={semester}
+              handleSave={handleSave}
+              handleRowToggle={handleRowToggle}
+              addRowToggle={addRowToggle}
             />
-            <SemesterTableBody tableRows={tableRows} semester={semester} handleSave={handleSave} 
-                handleRowToggle={handleRowToggle} addRowToggle={addRowToggle}/>
           </Table>
         </TableContainer>
-      </Paper>      
-        { !addRowToggle && <Button variant="outlined" onClick={handleRowToggle} startIcon={<AddOutlinedIcon /> }>
-        הוסף קורס חדש
-        </Button> }
+      </Paper>
+      {!addRowToggle && (
+        <Button variant="outlined" onClick={handleRowToggle}>
+          הוסף קורס חדש
+        </Button>
+      )}
     </Box>
   );
-}
+};
 
 export const SemesterTable = observer(SemesterTableComp);
