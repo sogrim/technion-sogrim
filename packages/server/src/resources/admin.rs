846c735929c1c98fad9d8259f188c7622e06917c
benny-n <benick12@gmail.com>
2022-01-21 13:13:57 +0200

    create admin resource

---
+++ packages/server/src/resources/admin.rs
@@ -0,0 +1,21 @@
+use crate::db;
+use crate::impl_from_request;
+use crate::middleware::auth::Sub;
+use actix_web::dev::Payload;
+use actix_web::error::{ErrorInternalServerError, ErrorUnauthorized};
+use actix_web::{web, Error, FromRequest, HttpMessage, HttpRequest};
+use bson::doc;
+use futures_util::Future;
+use mongodb::Client;
+use serde::{Deserialize, Serialize};
+use std::pin::Pin;
+
+#[derive(Default, Clone, Debug, Deserialize, Serialize)]
+pub struct Admin {
+    #[serde(rename(serialize = "_id", deserialize = "_id"))]
+    pub sub: String,
+    pub institution: String,
+    pub faculty: String,
+}
+
+impl_from_request!(resource = Admin, getter = get_admin_by_id);
