7d39df15395a13109f93f9b10735d83ed1681cd3
nissano <nissano@wix.com>
2022-01-22 11:40:34 +0200

    catalog & courses api

---
+++ packages/sogrim-bo-app/src/services/api.ts
@@ -0,0 +1,139 @@
+import axios from "axios";
+import { API_URL } from "./api-url";
+
+/////////////////////////////////////////////////////////////////////////////
+// Courses API
+/////////////////////////////////////////////////////////////////////////////
+
+export const getCourses = async (authToken: any): Promise<any> => {
+  let fallback: any;
+  let res: any;
+  try {
+    res =
+      (await axios.get(`${API_URL}/courses`, {
+        headers: {
+          authorization: `${authToken}`,
+        },
+      })) || fallback;
+  } catch {
+    res = fallback;
+  }
+  return res.data;
+};
+
+export const getCourse = async (
+  authToken: any,
+  courseId: string
+): Promise<any> => {
+  let fallback: any;
+  let res: any;
+  try {
+    res =
+      (await axios.get(`${API_URL}/courses/${courseId}`, {
+        headers: {
+          authorization: `${authToken}`,
+        },
+      })) || fallback;
+  } catch {
+    res = fallback;
+  }
+  return res.data;
+};
+
+export const updateCourse = async (
+  authToken: any,
+  courseId: string,
+  course: any
+): Promise<any> => {
+  let fallback: any;
+  let res: any;
+  try {
+    res =
+      (await axios.put(`${API_URL}/courses/${courseId}`, course, {
+        headers: {
+          authorization: `${authToken}`,
+        },
+      })) || fallback;
+  } catch {
+    res = fallback;
+  }
+  return res.data;
+};
+
+export const deleteCourse = async (
+  authToken: any,
+  courseId: string
+): Promise<any> => {
+  let fallback: any;
+  let res: any;
+  try {
+    res =
+      (await axios.delete(`${API_URL}/courses/${courseId}`, {
+        headers: {
+          authorization: `${authToken}`,
+        },
+      })) || fallback;
+  } catch {
+    res = fallback;
+  }
+  return res.data;
+};
+
+/////////////////////////////////////////////////////////////////////////////
+// Catalogs API
+/////////////////////////////////////////////////////////////////////////////
+
+export const getCatalogs = async (authToken: any): Promise<any> => {
+  let fallback: any;
+  let res: any;
+  try {
+    res =
+      (await axios.get(`${API_URL}/catalogs`, {
+        headers: {
+          authorization: `${authToken}`,
+        },
+      })) || fallback;
+  } catch {
+    res = fallback;
+  }
+  return res.data;
+};
+
+export const getCatalog = async (
+  authToken: any,
+  catalogId: string
+): Promise<any> => {
+  let fallback: any;
+  let res: any;
+  try {
+    res =
+      (await axios.get(`${API_URL}/catalogs/${catalogId}`, {
+        headers: {
+          authorization: `${authToken}`,
+        },
+      })) || fallback;
+  } catch {
+    res = fallback;
+  }
+  return res.data;
+};
+
+export const updateCatalog = async (
+  authToken: any,
+  catalogId: string,
+  catalog: any
+): Promise<any> => {
+  let fallback: any;
+  let res: any;
+  try {
+    res =
+      (await axios.put(`${API_URL}/catalogs/${catalogId}`, catalog, {
+        headers: {
+          authorization: `${authToken}`,
+        },
+      })) || fallback;
+  } catch {
+    res = fallback;
+  }
+  return res.data;
+};
