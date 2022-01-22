a6ebaf1154bf86aca217df91d9f16c4b546bdb8a
nissano <nissano@wix.com>
2022-01-22 12:36:28 +0200

    data types from db

---
+++ packages/sogrim-bo-app/src/types/data-types.ts
@@ -0,0 +1,44 @@
+export type Course = {
+  _id: string;
+  credit: number;
+  name: string;
+};
+
+export type Catalog = {
+  _id: string;
+  name: string;
+  total_credit: string;
+  description: string;
+  course_banks: CourseBank[];
+  credit_overflows: CreditOverFlows[];
+  course_to_bank: { [key: string]: string }; // Mapping from CourseToBank object to mapping object.
+  catalog_replacements: { [key: string]: { [key: number]: string }[] };
+  common_replacements: { [key: string]: { [key: number]: string }[] };
+};
+
+// TODO: add קבוצות התמחות
+export type CourseBank = {
+  name: string;
+  rule:
+    | "All"
+    | "AccumulateCredit"
+    | "Malag"
+    | "Sport"
+    | "Elective"
+    | AccumulateCoursesRule
+    | ChainRule;
+  credit: number;
+};
+
+export type CreditOverFlows = {
+  from: string;
+  to: string;
+};
+
+export type AccumulateCoursesRule = {
+  AccumulateCourses: number;
+};
+
+export type ChainRule = {
+  Chains: string[][]; // Each string is course id
+};
