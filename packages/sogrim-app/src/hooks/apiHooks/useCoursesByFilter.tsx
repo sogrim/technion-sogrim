83bdcc71b5e35af82dd2f1b95360f6d004fc8a6a
benny-n <benick12@gmail.com>
2022-02-06 16:36:03 +0200

    add client-side API + hook to get courses by filter

---
+++ packages/sogrim-app/src/hooks/apiHooks/useCoursesByFilter.tsx
@@ -0,0 +1,18 @@
+import { useQuery } from "react-query";
+import { getCourseByFilter } from "../../services/api";
+
+export default function useCoursesByFilter(
+  authToken: any,
+  trigger: boolean,
+  filterName: string,
+  filter: string
+) {
+  return useQuery(
+    "coursesByFilter",
+    () => getCourseByFilter(authToken, filterName, filter),
+    {
+      retry: false,
+      enabled: !!authToken && trigger,
+    }
+  );
+}
