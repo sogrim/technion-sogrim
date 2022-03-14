0fec6030609407ae8f7e1daf014112e907013c25
benny-n <benick12@gmail.com>
2022-03-14 19:19:18 +0200

    make CORS more restrictive

---
+++ packages/server/src/cors.rs
@@ -0,0 +1,17 @@
+use crate::config::CONFIG;
+use actix_cors::Cors;
+use actix_web::http::header;
+
+pub fn cors() -> actix_cors::Cors {
+    let cors = Cors::default()
+        .allowed_methods(vec!["GET", "POST", "PUT", "DELETE"])
+        .allowed_headers(&[header::AUTHORIZATION]);
+    if CONFIG.profile == "debug" {
+        cors.allowed_origin_fn(|origin, _req_head| {
+            origin.as_bytes().starts_with(b"http://localhost")
+        })
+    } else {
+        cors.allowed_origin("https://sogrim.org")
+            .allowed_origin("https://students.sogrim.org")
+    }
+}
