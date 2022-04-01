88372392c282ae36832550b2360921dac0aca726
benny-n <benick12@gmail.com>
2022-04-01 14:59:23 +0300

    minor color change in linear progress bar

--- packages/sogrim-app/src/components/Banner/BannerCards/LinearProgressWithLabel.tsx
+++ packages/sogrim-app/src/components/Banner/BannerCards/LinearProgressWithLabel.tsx
@@ -1,16 +1,29 @@
+import Box from "@mui/material/Box";
 import LinearProgress, {
+  linearProgressClasses,
   LinearProgressProps,
 } from "@mui/material/LinearProgress";
+import { useTheme } from "@mui/material/styles";
 import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
 
 export const LinearProgressWithLabel = (
   props: LinearProgressProps & { value: number }
 ) => {
+  const theme = useTheme();
+
   return (
     <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
       <Box sx={{ width: "100%" }}>
-        <LinearProgress variant="determinate" {...props} color="secondary" />
+        <LinearProgress
+          {...props}
+          variant="determinate"
+          sx={{
+            backgroundColor: "rgba(0, 0, 0, 0.178)",
+            [`& .${linearProgressClasses.bar}`]: {
+              backgroundColor: theme.palette.secondary.main,
+            },
+          }}
+        />
       </Box>
       <Box sx={{ minWidth: 35, mr: -1 }}>
         <Typography variant="body2" color="text.secondary">
