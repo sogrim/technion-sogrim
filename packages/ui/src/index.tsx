4f153e726e00f0ec1776133df6c234babf6c3990
benny-n <benick12@gmail.com>
2022-09-09 02:00:02 +0300

    Add turborepo boilerplate config files

---
+++ packages/ui/src/index.tsx
@@ -0,0 +1,7 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import * as React from "react";
+
+// Create a new file in the same directory as this file called "MyComponent.tsx"
+// Uncomment the following line and save the file
+
+//export * from "./MyComponent";
