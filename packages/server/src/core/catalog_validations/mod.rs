b69a8fb12f68ab9e42b8e0b33fe7b36b04238ed5
Liad Aram <liadaram1@gmail.com>
2022-09-10 20:20:52 +0300

    .

--- packages/server/src/core/catalog_validations/mod.rs
+++ packages/server/src/core/catalog_validations/mod.rs
@@ -1,13 +1,8 @@
-pub mod specialization_groups_validations;
-
 use crate::{error::AppError, resources::catalog::Catalog};
-
-use self::specialization_groups_validations::fix_specialization_groups;
 
 use super::credit_transfer_graph::validate_acyclic_credit_transfer_graph;
 
 pub fn validate_catalog(mut catalog: Catalog) -> Result<Catalog, AppError> {
     validate_acyclic_credit_transfer_graph(&catalog)?;
-    fix_specialization_groups(&mut catalog);
     Ok(catalog)
 }
