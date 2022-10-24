f567cf04f3151fee43c67ecc6e948bf119af9c17
benny-n <benick12@gmail.com>
2022-10-25 02:47:50 +0300

    Hygienise `from_request!` macro

--- packages/server/src/middleware/from_request.rs
+++ packages/server/src/middleware/from_request.rs
@@ -1,33 +1,38 @@
 #[macro_export]
 macro_rules! impl_from_request {
     (for $resource:ty) => {
-        impl FromRequest for $resource {
-            type Error = AppError;
-            type Future = Pin<Box<dyn Future<Output = Result<Self, Self::Error>>>>;
+        impl actix_web::FromRequest for $resource {
+            type Error = $crate::error::AppError;
+            type Future =
+                std::pin::Pin<Box<dyn std::future::Future<Output = Result<Self, Self::Error>>>>;
 
-            fn from_request(req: &HttpRequest, _: &mut Payload) -> Self::Future {
+            fn from_request(
+                req: &actix_web::HttpRequest,
+                _: &mut actix_web::dev::Payload,
+            ) -> Self::Future {
                 let req = req.clone();
                 Box::pin(async move {
-                    let db = match req.app_data::<Data<Db>>() {
+                    let db = match req.app_data::<actix_web::web::Data<$crate::db::Db>>() {
                         Some(db) => db,
                         None => {
-                            return Err(AppError::InternalServer(
+                            return Err($crate::error::AppError::InternalServer(
                                 "Mongodb client not found in application data".into(),
                             ))
                         }
                     };
-                    let optional_sub = req.extensions().get::<Sub>().cloned();
+                    use actix_web::HttpMessage; // Required for `req.extensions()`
+                    let optional_sub = req.extensions().get::<$crate::auth::Sub>().cloned();
                     match optional_sub {
                         Some(key) => db.get::<$resource>(&key).await,
-                        None => Err(AppError::Middleware(
+                        None => Err($crate::error::AppError::Middleware(
                             "Sub not found in request extensions".into(),
                         )),
                     }
                 })
             }
 
-            fn extract(req: &HttpRequest) -> Self::Future {
-                Self::from_request(req, &mut Payload::None)
+            fn extract(req: &actix_web::HttpRequest) -> Self::Future {
+                Self::from_request(req, &mut actix_web::dev::Payload::None)
             }
         }
     };
--- packages/server/src/resources/user.rs
+++ packages/server/src/resources/user.rs
@@ -1,16 +1,6 @@
 use super::catalog::DisplayCatalog;
-use crate::core::degree_status::DegreeStatus;
-use crate::db::CollectionName;
-use crate::error::AppError;
-use crate::impl_from_request;
-use crate::middleware::auth::Sub;
-use crate::Db;
-use actix_web::dev::Payload;
-use actix_web::{web::Data, FromRequest, HttpMessage, HttpRequest};
-use bson::doc;
-use futures_util::Future;
+use crate::{core::degree_status::DegreeStatus, db::CollectionName, impl_from_request};
 use serde::{Deserialize, Serialize};
-use std::pin::Pin;
 
 #[derive(Default, Clone, Debug, Deserialize, Serialize)]
 pub struct UserDetails {
