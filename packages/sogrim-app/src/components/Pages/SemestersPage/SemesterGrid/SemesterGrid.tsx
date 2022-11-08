4801953f2efd589f8513a7b79b7a8ef8a01695e9
benny-n <benick12@gmail.com>
2022-11-08 02:11:58 +0200

    Only mutate user details if row actually changed

--- packages/sogrim-app/src/components/Pages/SemestersPage/SemesterGrid/SemesterGrid.tsx
+++ packages/sogrim-app/src/components/Pages/SemestersPage/SemesterGrid/SemesterGrid.tsx
@@ -129,7 +129,7 @@ const SemesterGridComp: React.FC<SemesterGridProps> = ({ semester }) => {
           newUserDetails = deleteCourseInUserDetails(rowData, semester);
           break;
       }
-      mutate(newUserDetails);
+      newUserDetails.modified && mutate(newUserDetails);
     },
     [
       deleteCourseInUserDetails,
