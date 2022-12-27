eb7da305af8fd7906feac0e372f421b294d044a4
benny-n <benick12@gmail.com>
2022-12-27 22:29:15 +0200

    Refactor certain parts to be more rust-idiomatic

--- packages/server/src/core/bank_rule/all.rs
+++ packages/server/src/core/bank_rule/all.rs
@@ -11,25 +11,26 @@ impl<'a> BankRuleHandler<'a> {
 
         // handle courses in course list which the user didn't complete or any replacement for them
         // If the user didn't complete one of the courses requirements the bank is not completed
-        for course_id in self.course_list.iter() {
-            if !credit_info.handled_courses.contains_key(course_id) {
-                let course = if let Some(course) = self.courses.get(course_id) {
-                    course.clone()
-                } else {
-                    Course {
+        self.course_list
+            .iter()
+            .filter(|&course_id| !credit_info.handled_courses.contains_key(course_id))
+            .for_each(|course_id| {
+                let course = self
+                    .courses
+                    .get(course_id)
+                    .cloned()
+                    .unwrap_or_else(|| Course {
                         id: course_id.clone(),
                         credit: 0.0,
                         name: messages::cannot_find_course(),
-                    }
-                };
+                    });
                 self.degree_status.course_statuses.push(CourseStatus {
                     course,
                     state: Some(CourseState::NotComplete),
                     r#type: Some(self.bank_name.clone()),
                     ..Default::default()
                 });
-            }
-        }
+            });
 
         for course_status in self.degree_status.course_statuses.iter() {
             if course_status.r#type == Some(self.bank_name.clone()) {
