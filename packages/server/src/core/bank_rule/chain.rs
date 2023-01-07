8ae64b3a9071eb526ff0d22f0b3dd1ed4cb080c8
benny-n <benick12@gmail.com>
2023-01-07 18:39:36 +0200

    Refactor `chain` bank rule handler

--- packages/server/src/core/bank_rule/chain.rs
+++ packages/server/src/core/bank_rule/chain.rs
@@ -1,41 +1,35 @@
-use crate::core::types::Chain;
+use crate::{
+    core::types::Chain,
+    resources::course::{CourseId, CourseStatus},
+};
 
 use super::BankRuleHandler;
 
 impl<'a> BankRuleHandler<'a> {
     pub fn chain(mut self, chains: &[Chain], chain_done: &mut Vec<String>) -> f32 {
         let credit_info = self.iterate_course_list();
-        chains
-            .iter()
-            // Check if all courses in the chain are completed
-            .filter(|chain| {
-                chain.iter().all(|course_id| {
-                    credit_info
-                        .handled_courses
-                        .get(course_id)
-                        .and_then(|course_id| {
-                            self.degree_status
-                                .get_course_status(course_id)
-                                .map(|course_status| course_status.completed())
-                        })
-                        .unwrap_or(false)
-                })
-            })
-            // Map the course ids in the chain to course names
-            .for_each(|chain| {
-                chain.iter().for_each(|course_id| {
-                    credit_info
-                        .handled_courses
-                        .get(course_id)
-                        .and_then(|course_id| {
-                            self.degree_status
-                                .get_course_status(course_id)
-                                .map(|course_status| {
-                                    chain_done.push(course_status.course.name.clone());
-                                })
-                        });
-                });
+        let map_to_actual_course = |course_id: &CourseId| -> Option<&CourseStatus> {
+            credit_info
+                .handled_courses
+                .get(course_id)
+                .and_then(|course_id| self.degree_status.get_course_status(course_id))
+        };
+        for chain in chains {
+            let chain_complete = chain.iter().all(|course_id| {
+                map_to_actual_course(course_id)
+                    .map(|course_status| course_status.completed())
+                    .unwrap_or(false)
             });
+            if chain_complete {
+                *chain_done = chain
+                    .iter()
+                    .filter_map(|course_id| map_to_actual_course(course_id))
+                    .map(|course_status| course_status.course.name.clone())
+                    .collect();
+
+                break;
+            }
+        }
 
         credit_info.sum_credit
     }
