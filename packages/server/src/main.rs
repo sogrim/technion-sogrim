6a05e67dbb5adf92ac2cd914ba8e04866cb4e516
Benny Nazimov <benick12@gmail.com>
2023-02-19 00:26:31 +0200

    Add healthcheck

--- packages/server/src/main.rs
+++ packages/server/src/main.rs
@@ -1,8 +1,9 @@
 use crate::config::CONFIG;
-use actix_web::{web, App, HttpServer};
+use actix_web::{web, App, HttpResponse, HttpServer};
 use actix_web_lab::middleware::from_fn;
 use db::Db;
 use dotenvy::dotenv;
+use error::AppError;
 use middleware::auth;
 
 mod api;
@@ -31,21 +32,30 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .app_data(web::Data::new(db.clone()))
             .app_data(auth::JwtDecoder::new())
-            .wrap(from_fn(auth::authenticate))
             .wrap(cors::cors())
             .wrap(logger::init_actix_logger())
-            .service(api::students::get_catalogs)
-            .service(api::students::login)
-            .service(api::students::update_catalog)
-            .service(api::students::get_courses_by_filter)
-            .service(api::students::add_courses)
-            .service(api::students::compute_degree_status)
-            .service(api::students::update_details)
-            .service(api::students::update_settings)
-            .service(api::bo::get_all_courses)
-            .service(api::bo::get_course_by_id)
-            .service(api::bo::create_or_update_course)
-            .service(api::bo::delete_course)
+            .service(web::resource("/healthcheck").route(web::get().to(
+                |db: web::Data<Db>| async move {
+                    db.ping().await?;
+                    Result::<HttpResponse, AppError>::Ok(HttpResponse::Ok().finish())
+                },
+            )))
+            .service(
+                web::scope("")
+                    .wrap(from_fn(auth::authenticate))
+                    .service(api::students::get_catalogs)
+                    .service(api::students::login)
+                    .service(api::students::update_catalog)
+                    .service(api::students::get_courses_by_filter)
+                    .service(api::students::add_courses)
+                    .service(api::students::compute_degree_status)
+                    .service(api::students::update_details)
+                    .service(api::students::update_settings)
+                    .service(api::bo::get_all_courses)
+                    .service(api::bo::get_course_by_id)
+                    .service(api::bo::create_or_update_course)
+                    .service(api::bo::delete_course),
+            )
     })
     .bind((CONFIG.ip, CONFIG.port))?
     .run()
