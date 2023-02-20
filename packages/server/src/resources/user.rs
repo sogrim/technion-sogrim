b2793d4db16c180a1422b6a0e52cd18541470aa3
Benny Nazimov <benick12@gmail.com>
2023-02-20 19:04:10 +0200

    Remove `impl_from_request!` macro

--- packages/server/src/resources/user.rs
+++ packages/server/src/resources/user.rs
@@ -1,5 +1,11 @@
 use super::catalog::DisplayCatalog;
-use crate::{core::degree_status::DegreeStatus, db::Resource, impl_from_request};
+use crate::{
+    core::degree_status::DegreeStatus,
+    db::{Db, Resource},
+    error::AppError,
+    middleware::auth::Sub,
+};
+use actix_web::{dev::Payload, web::Data, FromRequest};
 use bson::{doc, Document};
 use serde::{Deserialize, Serialize};
 
@@ -42,4 +48,49 @@ impl Resource for User {
     }
 }
 
-impl_from_request!(for User);
+impl FromRequest for User {
+    type Error = AppError;
+    type Future = std::pin::Pin<Box<dyn std::future::Future<Output = Result<Self, Self::Error>>>>;
+    fn from_request(req: &actix_web::HttpRequest, _: &mut Payload) -> Self::Future {
+        let req = req.clone();
+        Box::pin(async move {
+            let db = match req.app_data::<Data<Db>>() {
+                Some(db) => db,
+                None => {
+                    return Err(AppError::InternalServer(
+                        "Mongodb client not found in application data".into(),
+                    ))
+                }
+            };
+            use actix_web::HttpMessage;
+            let optional_sub = req.extensions().get::<Sub>().cloned();
+            let user = match optional_sub {
+                Some(key) => db.get::<User>(&key).await,
+                None => Err(AppError::Middleware(
+                    "Sub not found in request extensions".into(),
+                )),
+            }?;
+            let is_authorized = match (req.path(), user.permissions) {
+                (path, permissions) if path.starts_with("/student") => {
+                    permissions >= Permissions::Student
+                }
+                (path, permissions) if path.starts_with("/admin") => {
+                    permissions >= Permissions::Admin
+                }
+                (path, permissions) if path.starts_with("/owner") => {
+                    permissions >= Permissions::Owner
+                }
+                _ => false,
+            };
+            if !is_authorized {
+                return Err(AppError::Unauthorized(
+                    "User not authorized to access this resource".into(),
+                ));
+            }
+            Ok(user)
+        })
+    }
+    fn extract(req: &actix_web::HttpRequest) -> Self::Future {
+        Self::from_request(req, &mut Payload::None)
+    }
+}
