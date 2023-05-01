6d238f00030c49819ae166e4fa3a207d7d0ecb33
Benny Nazimov <benick12@gmail.com>
2023-05-01 21:03:49 +0300

    Add explanation comments to `build.rs`

--- packages/server/build.rs
+++ packages/server/build.rs
@@ -1,13 +1,17 @@
 use dotenvy::{dotenv, dotenv_iter};
 
 fn main() {
-    let Ok(dotenv_path) = dotenv() else {
-        // This is not an error, it just means we are in a production environment.
+    let (Ok(dotenv_path), Ok(mut dotenv_iter)) = (dotenv(), dotenv_iter()) else {
+        // In this case, we don't have a .env file and we should assume that the environment variables are already set
+        // NOTE: This is NOT an error, it just means we are in a production or CI environment
         return;
     };
+
+    // rerun-if-changed is used to tell cargo to re-run this build script if the .env file changes
     println!("cargo:rerun-if-changed={}", dotenv_path.display());
-    let mut env_var_iter = dotenv_iter().expect("failed to read .env file");
-    while let Some(Ok((key, value))) = env_var_iter.next() {
+
+    // Set the environment variables
+    while let Some(Ok((key, value))) = dotenv_iter.next() {
         println!("cargo:rustc-env={}={}", key, value);
     }
 }
