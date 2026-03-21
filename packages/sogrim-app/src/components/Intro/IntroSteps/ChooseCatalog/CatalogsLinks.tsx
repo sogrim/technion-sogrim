b6f705f03eda9490dfffab1e023257e8307d2819
liadaram <liadaram1@gmail.com>
2026-03-21 17:57:17 +0200

    update catalog links

--- packages/sogrim-app/src/components/Intro/IntroSteps/ChooseCatalog/CatalogsLinks.tsx
+++ packages/sogrim-app/src/components/Intro/IntroSteps/ChooseCatalog/CatalogsLinks.tsx
@@ -3,20 +3,20 @@ import { Box, Link, Theme } from "@mui/material";
 
 export const catalogsLinks = [
   {
-    link: "https://ugportal.technion.ac.il/קטלוג-לימודים-תשף-2019-20/",
-    year: "תש”ף 2019/20",
-  },
-  {
-    link: "https://ugportal.technion.ac.il/קטלוג-לימודים-תשפא-2020-21/",
-    year: "תשפ”א 2020/21",
-  },
-  {
-    link: "https://ugportal.technion.ac.il/קטלוג-לימודים-תשפב-2021-22/",
+    link: "https://cs.technion.ac.il/he/undergraduate/programs/catalogs/cs_catalog_hebrew2021-2022.pdf",
     year: "תשפ”ב 2021/22",
   },
   {
-    link: "https://ugportal.technion.ac.il/קטלוג-לימודים-תשפג-2022-23/",
+    link: "https://cs.technion.ac.il/he/undergraduate/programs/catalogs/cs_catalog_2022_2023.pdf",
     year: "תשפ”ג 2022/23",
+  },
+  {
+    link: "https://undergraduate.cs.technion.ac.il/wp-content/uploads/2023/12/23-%D7%9E%D7%93%D7%A2%D7%99-%D7%94%D7%9E%D7%97%D7%A9%D7%91-%D7%AA%D7%A9%D7%A4%D7%B4%D7%93.pdf",
+    year: "תשפ”ד 2023/24",
+  },
+  {
+    link: "https://undergraduate.cs.technion.ac.il/wp-content/uploads/2024/12/23-%D7%94%D7%A4%D7%A7%D7%95%D7%9C%D7%98%D7%94-%D7%9C%D7%9E%D7%93%D7%A2%D7%99-%D7%94%D7%9E%D7%97%D7%A9%D7%91-%D7%AA%D7%A9%D7%A4%D7%B4%D7%94-.pdf",
+    year: "תשפ”ה 2024/25",
   },
 ];
 
